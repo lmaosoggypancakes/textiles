b1ac5cff6753343b3b485cd144059f7ddf8dba9d
coder6583 <snarita@andrew.cmu.edu>
2024-07-18 01:24:08 -0400

    test

--- parse_footprint.py
+++ parse_footprint.py
@@ -138,7 +138,7 @@ def extract_footprint(src):
                                       "end": {"x": line.end.x[0], "y": line.end.y[0]}, 
                                       "stroke": {"width": line.stroke.width[0], "type": line.stroke.type}, 
                                       "layer": line.layer, "uuid": line.uuid}, footprint.lines))
-  paths = list(map(lambda path: {"start": path.start, "end": path.end}), filter(lines_data, ))
+  paths = list(map(lambda path: {"start": path.start, "end": path.end}, lines_data))
   print(lines_data)
   print(pins)
 
